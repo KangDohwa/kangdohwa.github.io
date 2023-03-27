3130a8972731e647a5cd1d966091255eeac0098e
KangDohwa <kangdohwa0310@gmail.com>
2023-03-27 19:49:17 +0900

    desc update

--- src/component/SheetGenerator/Core.js
+++ src/component/SheetGenerator/Core.js
@@ -334,8 +334,11 @@ function MyPage() {
         <div className = "Admin-Desc">
           <p>안녕하세요! 사이트를 만든 쩨 입니다!</p>
           <p>트친소 생성기를 직접 만들어보고 싶어서 열심히 만들어봤습니다 :D</p>
-          <p>취미삼아 만들어본지라 버그가 있을수도 있으며, 테스트는 데스크톱 크롬에서만 진행했습니다!</p>
-          <p>버그 제보나 기능 제안, 문의사항 등은 <a href = "https://www.twitter.com/Jjae_ff14">트위터 @Jjae_ff14</a>로 멘션해주시거나 DM 보내주세요! </p>
+          <p>테스트를 충분히 거치지 않았으며, 데스크톱 크롬 환경에서만 확인해봤습니다.</p>
+          <p>모바일 환경에서는 사용이 어려우니 양해 부탁드립니다! 추후 추가해보도록 노력해볼게요!</p>
+          <p>버그 제보나 기능 제안, 문의사항 등은 <a
+            href = "https://www.twitter.com/Jjae_ff14" target = "_blank" rel="noreferrer">트위터 @Jjae_ff14</a>
+            로 멘션해주시거나 DM 보내주세요! </p>
         </div>
         <div className = "preview"> {/* image upload/download button div */}
           <FileInput label = "사진 업로드" onChange = {onImageChange} comment = "현재 16:9 비율의 사진만 지원합니다."/>
