6ba5ee66419d0e3d04bde3269d95ad12ba6899a2
KangDohwa <kangdohwa0310@gmail.com>
2023-04-15 22:55:29 +0900

    chore: fix proxy error

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,19 +2,19 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
-    createProxyMiddleware("/eorzeacollection", {
-      target: "https://ffxiv.eorzeacollection.com",
-      pathRewrite: {
-        "^/eorzeacollection": ""
-      },
-      changeOrigin: true
-    }),
-    createProxyMiddleware("/ver", {
-      target: "https://ffxiv.gamerescape.com",
-      pathRewrite: {
-        "^/ver": ""
-      },
-      changeOrigin: true
-    }),
+    // createProxyMiddleware("/eorzeacollection", {
+    //   target: "https://ffxiv.eorzeacollection.com",
+    //   pathRewrite: {
+    //     "^/eorzeacollection": ""
+    //   },
+    //   changeOrigin: true
+    // }),
+    // createProxyMiddleware("/ver", {
+    //   target: "https://ffxiv.gamerescape.com",
+    //   pathRewrite: {
+    //     "^/ver": ""
+    //   },
+    //   changeOrigin: true
+    // }),
   )
 }
